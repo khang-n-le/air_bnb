fea5996e1262c198bfde04b5dabdca39b0de8d60
khang-n-le <khang.ln.ueh@gmail.com>
2023-08-03 22:17:43 +0700

    update-reponsive

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,19 @@
+import { DEVICES } from 'utils';
+import Desktop from './Desktop';
+import Mobile from './Mobile';
+import { useAppSelector } from 'app/hooks';
+import { selectAppDevice } from 'slice/appDeviceSlice';
+
 const Header = () => {
-  return <>Header</>;
+  const appDevice = useAppSelector(selectAppDevice);
+
+  const renderElement = {
+    [DEVICES.DESKTOP]: <Desktop />,
+    [DEVICES.MOBILE]: <Mobile />,
+    [DEVICES.TABLET]: <Mobile />,
+  };
+
+  return renderElement[appDevice.device];
 };
 
 export default Header;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,9 @@
-import { Button } from 'antd';
 import React from 'react';
 import { ButtonWrapper, CButton, Wrapper } from './styled';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { selectAppDevice } from '../../slice/appDeviceSlice';
+import { useAppSelector } from 'app/hooks';
+import { selectAppDevice } from 'slice/appDeviceSlice';
 
 const Home = () => {
-  const appDevice = useAppSelector(selectAppDevice);
-
-  React.useEffect(() => {
-    console.log('data1', appDevice);
-  }, [appDevice]);
-
   return (
     <Wrapper>
       hello
