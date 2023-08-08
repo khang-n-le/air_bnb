2d9e94bc7e30db528716d675dbd231bef5754f30
khang-n-le <khang.ln.ueh@gmail.com>
2023-08-08 23:50:59 +0700

    update-link

--- src/pages/RoomDetail/index.tsx
+++ src/pages/RoomDetail/index.tsx
@@ -5,20 +5,26 @@ import Desktop from './Desktop';
 import Mobile from './Mobile';
 import React from 'react';
 import { getOneRoom } from 'slice/roomSilce';
+import { useLocation } from 'react-router-dom';
 
-const RoomDetail = () => {
+const RoomDetail = ({ match }: { match?: string }) => {
   const appDevice = useAppSelector(selectAppDevice);
   const [roomDetail, setRoomDetail] = React.useState({});
+  const location = useLocation();
   const dispatch = useAppDispatch();
 
   React.useEffect(() => {
     const getDetail = async () => {
-      const data = await dispatch(getOneRoom({ id: '1' })).unwrap();
+      const data = await dispatch(
+        getOneRoom({ id: location.pathname.split('/')[2] })
+      ).unwrap();
       setRoomDetail((data as any).content);
     };
 
-    getDetail();
-  }, []);
+    if (location.pathname?.split('/')[2]) {
+      getDetail();
+    }
+  }, [location.pathname?.split('/')[2]]);
 
   const RenderElement = {
     [DEVICES.DESKTOP]: <Desktop data={roomDetail}></Desktop>,
