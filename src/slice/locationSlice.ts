f76d1d0518c1ed2253d710df385c8607e755018f
khang-n-le <khang.ln.ueh@gmail.com>
2023-08-15 12:04:09 +0700

    update

--- src/slice/locationSlice.ts
+++ src/slice/locationSlice.ts
@@ -1,49 +1,47 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { FindOptions } from 'api/common';
-import { locationApi } from 'api/location';
+import { FindOneOptions, FindOptions } from 'api/common';
 import { RootState } from 'app/store';
 
 export const findAllLocation = createAsyncThunk(
-    'area/findAllLocation',
-    async (option: FindOptions, { rejectWithValue }) => {
-        try {
-            const response = await locationApi.findAll(option);
+  'area/findAllLocation',
+  async (option: FindOneOptions, { rejectWithValue }) => {
+    try {
+      // const response = await locationApi.findAll(option);
+      // return response;
+    } catch (error: any) {
+      if (!error.response) {
+        throw error;
+      }
 
-            return response;
-        } catch (error: any) {
-            if (!error.response) {
-                throw error;
-            }
-
-            return rejectWithValue(error.response.data);
-        }
+      return rejectWithValue(error.response.data);
     }
+  }
 );
 
 interface InitialState {
-    list: Array<any>;
+  list: Array<any>;
 }
 
 const initialState: InitialState = {
-    list: [],
+  list: [],
 };
 
 const locationSlice = createSlice({
-    name: 'location',
-    initialState,
-    reducers: {},
-    extraReducers: builder => {
-        builder.addCase(findAllLocation.fulfilled, (state, action) => {
-            state.list = (action.payload as any)?.content;
-        });
-    },
+  name: 'location',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(findAllLocation.fulfilled, (state, action) => {
+      state.list = (action.payload as any)?.content;
+    });
+  },
 });
 
 const { actions, reducer: locationReducer } = locationSlice;
 
-export const { } = actions;
+export const {} = actions;
 
 export default locationReducer;
 
 export const selectLocation = (state: RootState): InitialState =>
-    state.location as InitialState;
+  state.location as InitialState;
